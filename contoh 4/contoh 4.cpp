5a24c82dde51a6105f617c9bc80bcca2dbf7380b
avanronaufaladi <avaroadi@gmail.com>
2024-05-07 11:21:37 +0700

    memasukkan int

---
+++ contoh 4/contoh 4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private:
+	string nama;
+
+public:
+	friend void setnama(mahasiswa& a, string);
+};
+
+void setnama(mahasiswa& a, string pnama)
+{
+	a.nama = pnama;
+}
+
+int main()
+{
+	mahasiswa joko;
+	setnama(joko, "kairi kumar");
+	return 0;
+}
