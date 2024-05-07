9488b2a9ff88351ce612c131574d26d4495f3e5e
avanronaufaladi <avaroadi@gmail.com>
2024-05-07 11:24:17 +0700

    memasukkan include

--- contoh 4/contoh 4.cpp
+++ contoh 4/contoh 4.cpp
@@ -2,23 +2,3 @@
 #include <string>
 using namespace std;
 
-class mahasiswa
-{
-private:
-	string nama;
-
-public:
-	friend void setnama(mahasiswa& a, string);
-};
-
-void setnama(mahasiswa& a, string pnama)
-{
-	a.nama = pnama;
-}
-
-int main()
-{
-	mahasiswa joko;
-	setnama(joko, "kairi kumar");
-	return 0;
-}
