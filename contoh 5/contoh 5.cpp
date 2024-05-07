3d9f1567a2f50e8cbf5abdacc77c85e41a7233c9
avanronaufaladi <avaroadi@gmail.com>
2024-05-07 11:32:54 +0700

    memasukkan tampilan nilai pelajar

--- contoh 5/contoh 5.cpp
+++ contoh 5/contoh 5.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
+class pelajar;
+class manusia
+{
+public:
+	void shownilaipelajar(pelajar& x);
+};
 
+class pelajar
+{
+private:
+	int nilai;
+
+public:
+	pelajar() { nilai = 100; }
+	friend void manusia::shownilaipelajar(pelajar& x);
+
+};
+
+void manusia::shownilaipelajar(pelajar& x)
+{
+	cout << x.nilai;
+}
 
 int main()
 {
