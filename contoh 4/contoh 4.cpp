fcfb942a357cfbccee5641edd2cde17518693c58
avanronaufaladi <avaroadi@gmail.com>
2024-05-07 11:24:49 +0700

    memanggil nama mahasiswa

--- contoh 4/contoh 4.cpp
+++ contoh 4/contoh 4.cpp
@@ -2,3 +2,16 @@
 #include <string>
 using namespace std;
 
+class mahasiswa
+{
+private:
+	string nama;
+
+public:
+	friend void setnama(mahasiswa& a, string);
+};
+
+void setnama(mahasiswa& a, string pnama)
+{
+	a.nama = pnama;
+}
