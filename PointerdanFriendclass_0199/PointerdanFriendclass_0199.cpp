4b005497785f22d81ff6295b730fd5114bba3dcd
avanronaufaladi <avaroadi@gmail.com>
2024-05-07 10:41:52 +0700

    menampilkan nim mahasiswa

--- PointerdanFriendclass_0199/PointerdanFriendclass_0199.cpp
+++ PointerdanFriendclass_0199/PointerdanFriendclass_0199.cpp
@@ -10,6 +10,16 @@ public:
 	}
 };
 int main() {
-	
+	mahasiswa mhs{ 1 }; //object mhs
+	mhs.shownim(); // member acces operator
+
+	mahasiswa& refmhs = mhs; //pointer reference refmhs
+	refmhs.nim = 2; //member access operator
+	mhs.shownim();
+
+	mahasiswa* pmhs = &mhs; //pointer deference pmhs
+	pmhs->nim = 3; // arrow operator
+	pmhs->shownim();
+	return 0;
 
 }
